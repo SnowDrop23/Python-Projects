058ab23374094f646525b5dd10ea53e61d9a3f96
toma <144379705+SnowDrop23@users.noreply.github.com>
2026-01-05 21:09:53 +0600

    Delete A Simple Calculator.py

--- A Simple Calculator.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-
-ops = input("Enter operation(+, -, *, /) ")
-
-if ops == "+" : print(a, "+", b, "=", a+b)
-if ops == "-" : print(a, "-", b, "=", a-b)
-if ops == "*" : print(a, "*", b, "=", a*b)
-if ops == "/" : print(a, "/", b, "=", a/b)
