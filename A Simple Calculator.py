256688ae752867c19ba91d531d60f5639e455a64
toma <144379705+SnowDrop23@users.noreply.github.com>
2026-01-05 21:07:58 +0600

    Create A Simple Calculator.py

---
+++ A Simple Calculator.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+ops = input("Enter operation(+, -, *, /) ")
+
+if ops == "+" : print(a, "+", b, "=", a+b)
+if ops == "-" : print(a, "-", b, "=", a-b)
+if ops == "*" : print(a, "*", b, "=", a*b)
+if ops == "/" : print(a, "/", b, "=", a/b)
