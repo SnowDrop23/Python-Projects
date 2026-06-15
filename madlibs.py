ca07934e8b3ef95c20a827a67676537b53f32d23
Tomalika Paul Toma <144379705+SnowDrop23@users.noreply.github.com>
2026-06-15 13:03:09 +0600

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+print("-------Welcome to the game - Madlibs!-------")
+print("Fill in the blanks and get a funny story at the end!")
+print("Let's begin...\n")
+
+
+
+
+adj = input("Adjective: ")
+famous_person = input("Famous Person: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+adj2 = input("Adjective: ")
+
+print(f"School felt unusually {adj} today. Then {famous_person} entered the classroom and everyone went silent. Someone in the back whispered that this couldn't be real. They started to {verb1} in front of the teacher. I tried not to {verb2} because everything was getting too {adj2}. My heart was beating so fast I could barely sit still. After that, nobody in the room knew what would happen next.")
